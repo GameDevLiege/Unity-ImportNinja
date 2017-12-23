679ef93e950851563bb91447db1d23e92ad5c916
JeromeJ <jerome.jamart.42@gmail.com>
2017-12-23 17:26:27 +0100

    ImportNinja from directory template (not hardcoded anymore)

---
+++ ImportNinjaTemplate/_#PROJECTNAME#/Scripts/DualBehaviour.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class DualBehaviour : MonoBehaviour
+{
+    #region Public Members
+
+    #endregion
+
+    #region Public void
+
+    #endregion
+
+    #region System
+
+    protected virtual void Awake()
+    {
+        Init();
+    }
+
+    protected virtual void Reset()
+    {
+        Init();
+    }
+
+    #endregion
+
+    #region Class Methods
+
+	[ContextMenu("Generate Editor for this script")]
+	private void _GenerateEditor()
+	{
+        DirectoryInfo projectDirectory = Directory.GetParent(Application.dataPath);
+        string projectPath = Application.dataPath + "/_" + projectDirectory.Name;
+
+        // Didn't find a way to locate children class location using StackTrace, only DualBehaviours'
+        // Could theoretically find it using its classname and search for its name in the Asset folder
+
+        string name = this.GetType().Name;
+
+        Directory.CreateDirectory(projectPath);
+        Directory.CreateDirectory(projectPath + "/Editor");
+
+        // TODO: Create the Editor file here
+
+        AssetDatabase.Refresh();
+    }
+
+
+    private void Init()
+    {
+        if(m_transform == null)
+            m_transform = GetComponent<Transform>();
+    }
+
+
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+
+    [SerializeField]
+    protected Transform m_transform;
+
+    #endregion
+}
